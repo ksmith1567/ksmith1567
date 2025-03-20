a429dfe7fd271f8c40817a587426deca50c4a086
Kelly Smith <kesm1370@colorado.edu>
2025-03-19 22:02:52 -0600

    yeeeeee

---
+++ wa/wa10/wa10.js
@@ -0,0 +1,40 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+
+const storyText = "It was 800 fahrenheit outside, so :insertx: went for a swim. When they got to :inserty:, they sat in silence for a few moments, then :insertz:. Bob saw the whole thing and was shocked — :insertx: weighs 10 pounds, and it was a cold day.";
+const insertX=["Snoop Dogg", "JoJo Siwa", "Santa"];
+const insertY= ["Denver", "the C4C", "Rockefeller Center"];
+const insertZ= ["spontaneously combusted", "melted into a puddle on the driveway", "erupted into flames"];
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  let xItem=randomValueFromArray(insertX);
+  let yItem=randomValueFromArray(insertY);
+  let zItem=randomValueFromArray(insertZ);
+
+  let newStory=storyText.replaceAll(":insertx:", xItem).replace(":inserty:",yItem).replace(":insertz:",zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory=newStory.replace("Bob", name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300/14);  //converting from pounds to stones, divide by 14
+    const temperature =  Math.round((94-32)*(5/9)); //converting from fahrenheit into centigrade, F-32 times 5/9
+    newStory=newStory.replace("94 fahrenheit", `${temperature} centigrade`).replace("300 pounds", `${weight} stone`);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
